ece9d2b9d424b9e82ebb8a1fdcd28b6894d3d554
Seidor <Seidor@NTBSBVER86>
2022-11-02 13:57:20 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Serialization;
+
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+
+        var client = new ServiceReference1.SOAPDemoSoapClient();
+
+        var soma = await client.AddIntegerAsync(1, 5);
+        var division = await client.DivideIntegerAsync(5, 1);
+        var city = await client.LookupCityAsync("10001");
+
+
+        Console.WriteLine(soma);
+        Console.WriteLine(division);
+        Console.WriteLine(city.City);
+
+    }
+
+}
